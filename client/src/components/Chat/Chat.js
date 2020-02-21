059aaa8e8fb843367d883d1db04f291b218110a0
Jade Huynh <35jadeh@gmail.com>
2020-02-21 12:48:45 -0600

    infobar with icons

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import io from "socket.io-client";
 
 import './Chat.css';
 
+import InfoBar from '../InfoBar/InfoBar'
 let socket;
 
 const Chat = ({ location }) => {
@@ -51,7 +52,8 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
     <div className="container">
-        <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} />
+        <InfoBar room={room}/>
+        {/* <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} /> */}
     </div>
   </div>
   )
